bdd41dcb2303830e07b03b2cccb0f4dfd764e298
Scott <4062616+7ranceaddic7@users.noreply.github.com>
2020-09-01 16:00:57 -0700

    Rawhide!

--- dice_roller.py
+++ dice_roller.py
@@ -5,19 +5,19 @@ def main():
 
   os.system('cls')
 
-  dice_rolls = 2
-  dieSum = 0
+  dice_rolls = int(input("How many dice shall I roll? "))
+  dice_size = int(input("How many sides to each die? "))
+  dice_sum = 0
 
   for i in range(0,dice_rolls):
-    roll = random.randint(1,6)
-    dieSum += roll
-    print(f'You rolled a {roll}')
-  if dieSum==2:
+    roll = random.randint(1,dice_size)
+    dice_sum += roll
+    print(f'You rolled a {roll} and {roll} and {roll}')
+  if dice_sum==(dice_size):
     print('You rolled SnakeEyes! Critical Fail!')
-  elif dieSum==12:
-      print(f'You rolled a total of {dieSum}'" Critical HIT!")
+  elif dice_sum==(dice_rolls*dice_size):
+      print(f'You rolled a total of {dice_sum}'" Critical HIT!")
   else:
-    print(f'You rolled a total of {dieSum}')
-
+    print(f'You rolled a total of {dice_sum}')
 if __name__== "__main__":
   main()
