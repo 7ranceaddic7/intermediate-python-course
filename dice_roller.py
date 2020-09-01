ed29b00c9a45e52932d2f1b917692ddaccbd5b4f
Scott <4062616+7ranceaddic7@users.noreply.github.com>
2020-09-01 15:36:49 -0700

    Keep'em die a rollin'

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,19 @@
+import os
+import random
+
 def main():
-  print('You rolled a die')
+
+  os.system('cls')
+
+  dice_rolls = 2
+  dieSum = 0
+
+  for i in range(0,dice_rolls):
+    roll = random.randint(1,6)
+    dieSum += roll
+    print(f'You rolled a {roll}')
+  print(f'You rolled a total of {dieSum}')
+
 
 if __name__== "__main__":
   main()
